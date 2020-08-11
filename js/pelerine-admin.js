27142155de41976051d29d9296f53fc22423fae7
indigoxela <user@olga.indigofloat.at>
2020-08-11 08:42:49 +0200

    Add html5 colorpicker widget to css edit form item

--- js/pelerine-admin.js
+++ js/pelerine-admin.js
@@ -8,5 +8,15 @@
       imgUrl = Backdrop.settings.basePath + this.options[this.selectedIndex].value;
       $('#preview-wrapper').css('background-image', 'url("' + imgUrl + '")');
     });
+    // Color picker widget.
+    $('#html5colorpicker input').change(function() {
+      var picked = $(this).val();
+      var textAreaTxt = $("#edit-custom-css").val();
+      var cursorPos = $("#edit-custom-css").prop('selectionStart');
+      // Show hex value next to picker.
+      $('#html5colorpicker span').text(picked);
+      // Put into textarea at cursor position.
+      $("#edit-custom-css").val(textAreaTxt.substring(0, cursorPos) + picked + textAreaTxt.substring(cursorPos));
+    });
   });
 })(jQuery);
